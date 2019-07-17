6833704a97a3bae6c537460310a77ed152b28ad6
ParkJunyeong <49705282+heffy127@users.noreply.github.com>
2019-07-17 23:24:09 +0900

    2017-07-17

---
+++ P11_1/P11_1/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/*
+매개변수로 char*형 자료를 받아서
+문자열의 길이를 계산하여 반환하는 함수 작성
+함수의 이름은 GetLength()
+*/
+
+int GetLength(char *str)
+{
+	int index = 0;
+	while(str[index] != NULL)
+	{
+		index++;
+	}
+	return index;
+}
+
+int main(void)
+{
+	char word[10] = { "Hello" };
+	char word2[20] = { "parkjunyeong" };
+	char *myword = word;
+	char *myword2 = word2;
+	printf("Hello 길이 %d\n", GetLength(myword));
+	printf("parkjunyeong 길이 %d", GetLength(myword2));
+	return 0;
+}
---
+++ P11_4/P11_4/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/*
+정수를 입력받고, 그 개수만큼 char*를 
+여러개 저장할 수 있는 메모리를 동적 할당함
+그리고 입력할 문자열의 최대 길이를 입력받고
+최대 길이의 문자열을 저장할 수 있는 크기의 메모리를 동적 할당하여
+입력받은 문자열을 저장 후 출력
+*/
+
+int main(void)
+{
+	int num = 0;
+	int strLen = 0;
+
+	printf("정수 입력 : ");
+	scanf_s("%d", &num);
+	char **ppstr = (char**)malloc(sizeof(char*) * num); // 포인터 변수를 담는 포인터 변수
+
+	printf("문자열 최대 길이 입력 : ");
+	scanf_s("%d%*c", &strLen);
+
+	for (int i = 0; i < num; i++)
+	{
+		*(ppstr + i) = (char*)malloc(sizeof(char) * strLen);
+		gets_s(*(ppstr + i), strLen + 1);
+	}
+
+	for (int i = 0; i < num; i++)
+		printf("%s\n", ppstr[i]);
+
+	for (int i = 0; i < num; i++)
+		free(ppstr[i]);
+	free(ppstr);
+	
+	return 0;
+}
